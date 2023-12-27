5321b077d6d9077c55f482cff34dc4002e71fc83
Filip Stojanovic <filipmymail@gmail.com>
2023-12-27 01:44:26 +0100

    const value

--- public/js/makeElementsFunction.js
+++ public/js/makeElementsFunction.js
@@ -1,10 +1,10 @@
 // @license magnet:?xt=urn:btih:b8999bbaf509c08d127678643c515b9ab0836bae&dn=ISC.txt ISC
 function makeEl(tagName, attributes, listener) {
-    if (!Object.values(attributes).every(attribute => typeof attribute === 'string')) {
-        throw new Error('All attributes must be strings');
+    if (!Object.values(attributes).every(attribute => typeof attribute === "string")) {
+        throw new Error("All attributes must be strings");
     }
     
-    let el = document.createElement(tagName);
+    const el = document.createElement(tagName);
     Object.assign(el, attributes);
     el.addEventListener("click", listener);
 
